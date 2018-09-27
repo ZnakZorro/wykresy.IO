81e44563681ab4f8da88b82e956946ca2fd79cd3
Znak Zorro <ZnakZorro@users.noreply.github.com>
2018-09-27 12:37:13 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -16,24 +16,24 @@ var ttt=null;
 function slij(socket){
 	licz++
 	socket.broadcast.emit('drawing', los());
-	console.timeEnd('a');
-	console.time('a');
+	//console.timeEnd('a');
+	//console.time('a');
 }
 
 function onConnectionStart(socket){
-	console.log('onConnectionStart')
-	console.time('a');
+	//console.log('onConnectionStart')
+	//console.time('a');
 	onConnection(socket);
 	if (ttt===null) ttt=setInterval(function(){
 		slij(socket);
-		console.log(licz+" = ttt\n");
-	},2000);
+		//console.log(licz+" = ttt\n");
+	},15000);
 
 }
 
 function onConnection(socket){
   socket.on('drawing', (data) => {
-	  console.log(data);
+	  //console.log(data);
 	  socket.broadcast.emit('drawing', data)
   });
   
@@ -47,17 +47,26 @@ let cpu_command="grep 'cpu ' /proc/stat | awk '{usage=($2+$4)/($2+$4+$5)} END {p
 let cpu = execSync(cpu_command).pars();	
 //console.log(cpu,cpu_command)
 
-
 let tem_command="cat /sys/class/thermal/thermal_zone0/temp";
 let tem = execSync(tem_command).pars();	
 
+let wif_command="cat /proc/net/wireless | grep wlan0 | awk '{print $4}'";
+let wif = execSync(wif_command).pars();	
+
+
+
+
+
+
 let TEM = Math.round(parseFloat(tem)/10)/100;
 let CPU = Math.round(parseFloat(cpu*10000))/100;
-console.log('los=',TEM,CPU)
+let WIF = Math.round(parseFloat(wif));
+//console.log('los=',TEM,CPU)
 	return { 
 		time :(new Date()).getTime(),
 		cpu: CPU,
-		tem: TEM
+		tem: TEM,
+		wif: WIF
 	}
 }
 
